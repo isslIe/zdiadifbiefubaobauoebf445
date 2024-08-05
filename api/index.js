95ebd278c49f333dc499bc43cc18671ba12f42e3
s-ros <s.rossignol04@gmail.com>
2024-08-05 20:17:12 +0200

    nvirev

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
-const path = require('path')
-require('dotenv').config({ path: '../.env' });
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 const express = require('express');
 const helmet = require('helmet');
